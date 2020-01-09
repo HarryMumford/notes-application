60a6d4908524b7b7f144748932ed6976938361ac
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-01-09 11:37:41 +0000

    refactor

--- spec/single-note-view-spec.js
+++ spec/single-note-view-spec.js
@@ -1,9 +1,25 @@
+// describe('SingleNoteView', () => {
+//   describe('.toHTML', () => {
+//     it('converts note text to html', () => {
+//       var note = new Note('Hello I note')
+//       var singleNoteView = new SingleNoteView(note)
+//       expect(singleNoteView.toHTML()).toEq('<div>Hello I note</div>')
+//     })
+//   })
+// })
+
 describe('SingleNoteView', () => {
   describe('.toHTML', () => {
     it('converts note text to html', () => {
       var note = new Note('Hello I note')
       var singleNoteView = new SingleNoteView(note)
       expect(singleNoteView.toHTML()).toEq('<div>Hello I note</div>')
+    })
+
+    it('only displays the first 20 characters', () => {
+      var note = new Note('Hello I very long note')
+      var singleNoteView = new SingleNoteView(note)
+      expect(singleNoteView.toHTML()).toEq('<div>Hello I very long no...</div>')
     })
   })
 })
